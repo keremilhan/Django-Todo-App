2e7ec84fb01eb85908eff40fca737ebd107dd219
Kerem İlhan <99633775+keremilhan@users.noreply.github.com>
2024-05-17 00:11:13 +0200

    allowed hosts updated

--- main/settings.py
+++ main/settings.py
@@ -25,8 +25,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1','https://django-todo-app-zsqb.onrender.com']
-
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'django-todo-app-zsqb.onrender.com']
 
 # Application definition
 
